439de723ede08f49bc8b22ac7ded06e9acb98d83
KARLA ERICKA ERICKA <1719110219@utectulancingo.edu.mx>
2020-06-15 03:34:05 +0000

    3

--- semana_4/palindromo.py
+++ semana_4/palindromo.py
@@ -1,46 +1,34 @@
 
-class Palindromo: #Es para definir mi clase  
-    def _init_(self):#es el constructor 
+class palindromo1: #Es para definir mi clase  
+    def _init_(self):#definí mi constructor
         pass
     def palindromo(self): #formas derealizar comparaciones entre cadenas
         respuesta = "S" # es una respuesta que indica que estas en lo correcto o estas de acuerdo
         
-        while respuesta == "S" or respuesta== "s": # es la manera de decir si se vueleve a realizar otro analisis dela cadena 
-            cad = input("inserta tu cadena de texto: ") # Insertamos la cadena a analizar
-            numeroespacios= 0 # Almacena el numero de espacios
+        while respuesta == "S" or respuesta== "s": # mientras sea verdad iterara todo
+            cad = input("inserta tu cadena de texto: \n") # Insertamos la cadena a analizar
+            numeroespacios= 0 # Almacenara el numero de espacios iniciando mi contador en cero
             cad = cad.lower() # Reemplaza las mayusculas por minusculas
             for espacios in cad: # Analizara la cadena
                 if espacios in " ": #Cada espacio será contado
                     numeroespacios +=1
-            print("Numero de espacios: " +str(numeroespacios))
-            
-            cad = cad.lower() # Convierte todas las mayusculas en minusculas
-            cad= cad.replace(" ","") # Elimina espacios de la cadena
-                    # Cambia las letras con asento por una letra sin asento
-            cad = cad.replace("á", "a") 
-            cad = cad.replace("é", "e")
-            cad = cad.replace("í", "i")
-            cad = cad.replace("ó", "o")
-            cad = cad.replace("ú", "u")
-            numerovocales = 0 # Almacena el numero de Vocales
-            for vocales in cad: # Analiza todos los valores de la cadena
-                if vocales in "áéíóúaeiou":
+            print("Numero de espacios: " +str(numeroespacios)) 
+            cad = cad.lower().replace(" ","").replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u")# Convierte todas las mayusculas en minusculas,quita espacios, remplaza letras mayusculas por minusculas
+            numerovocales = 0 # Almacena el numero de vocales que encuentre una cadena de texto
+            for vocales in cad: # Analiza  los valores de la cadena de texto
+                if vocales in ("áéíóúaeiou"):
                     numerovocales += 1 # Cada vocal se contará
-            print("Numero de vocales: "+str(numerovocales))
-            
-            palindromo = "" # Almacena la cadena que se invertirá
-            for i in cad: # i tendra como inicio la ultima letra de la cadena hasta la primera
-                palindromo = i + palindromo # sera la cadena invertida que se obtuvo de la línea 33
-            if cad == palindromo: # Analiza si la cadena es un palindromo 
-                print("El texto es un Palindromo") # Nos afirma si la cadena es un palindromo
-            
-            else: 
-                
-                print("La cadena no es un palindromo") # Nos Niega que la cadena sea un Palindromo en caso de no cumplir con los requisitos
-            
-            respuesta= input("Desea anlizar otra cadena? s/n: ") # Pregunta para analizar otra cadena
-            if respuesta == "n" or respuesta == "N": # Si la respuesta es N se termina de ejecutar el analisis
+            print("Numero de vocales: ",numerovocales)
+            palindromo = "" #esta variable almacenara la cadena de texto invertida
+            for a in cad: #la variable a tendra como inicio de la cadena de texto
+                palindromo = a + str(palindromo) # se invertira la pocision de la cadena
+            if cad == palindromo: # el if analizara la cadena de texto
+                print("El texto es un Palindromo") # si el texto es un palindromo imprimira su veracidad
+            else:    
+                print("La cadena No es un palindromo") #si no esun palindromo imprimira que no es un palindromo
+            respuesta= input("Desea anlizar otra cadena? s/n: ") #pregunte si el usuario desea ingresar otra cadena de texto para iterarla
+            if respuesta == "n" or respuesta == "N": 
                 break #Fin del analisis
 
-textopalindromo = Palindromo()
+textopalindromo = palindromo1()
 textopalindromo.palindromo()
